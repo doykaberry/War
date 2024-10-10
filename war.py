57568a30ff657b146c135f568c15e5e456cb606d
doykaberry <jdc9229@gmail.com>
2024-10-10 11:50:51 -0400

    Update war.py

--- war.py
+++ war.py
@@ -20,3 +20,43 @@ def gameloop():
         print("p1 won")
     else:
         print("p2 won")
+
+
+#start of program
+
+#assignment of the card ranks and suits
+ranks = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+suits = ("hearts", "diamonds", "clubs", "spades")
+
+#assignment of the full deck
+deck = [(rank, suit) for rank in ranks for suit in suits]
+
+#deck shuffling
+import random
+def shuffled_deck():
+    random.shuffle(deck)
+    return deck
+    
+#deck splitting and assignment of both player's decks
+p1_deck = shuffled_deck()[0:26]
+p2_deck = shuffled_deck()[26:]
+
+#assignment of card in both player's hands
+def p1_card():
+    for card_p1 in range(len(p1_deck)):
+       p1_deck.pop(card_p1)
+
+p12_card = p1_deck.pop()
+p2_card = p2_deck.pop()
+
+
+
+
+
+
+
+
+
+
+
+
